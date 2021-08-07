b6a1a48b7580a507f22a16a95bc95ed9f95d328f
Ahmad Yahia <tabebqena@gmail.com>
2021-08-07 21:14:42 +0300

    export decorators

--- open_spec/__init__.py
+++ open_spec/__init__.py
@@ -1,3 +1,4 @@
 from .open_spec import OpenSpec  # noqa
 from .builder import OasBuilder  # noqa
 from .oas_config import OasConfig  # noqa
+from .decorators import request_body, parameter, path_details, response
