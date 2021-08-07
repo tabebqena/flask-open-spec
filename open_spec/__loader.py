f00c25436f0b0dd4bc2b2e0e252f42cc545f0e02
Ahmad Yahia <tabebqena@gmail.com>
2021-08-07 21:15:16 +0300

    change the parameters

--- open_spec/__loader.py
+++ open_spec/__loader.py
@@ -1,14 +1,14 @@
 from copy import deepcopy
-from open_spec.builder import OasBuilder
-from ._parameters import get_app_paths, extract_path_parameters
-
-from ._utils import load_file, merge_recursive
-
 from typing import TYPE_CHECKING
 
+from ._parameters import extract_path_parameters, get_app_paths
+from ._utils import load_file, merge_recursive
+from .builder import OasBuilder
+
 if TYPE_CHECKING:
-    from .open_spec import OpenSpec
+    from ._editor import TemplatesEditor
     from .oas_config import OasConfig
+    from .open_spec import OpenSpec
 
 
 def _load_or_fetch(save_files, file_path, fetcher, fetcher_kwargs={}):
@@ -17,13 +17,12 @@ def _load_or_fetch(save_files, file_path, fetcher, fetcher_kwargs={}):
     return fetcher(**fetcher_kwargs)
 
 
-def __load_data(open_spec: "OpenSpec"):
-    config = open_spec.config
+def __load_data(config: "OasConfig", editor: "TemplatesEditor"):
     draft_data = load_file(config.draft_file)
     paths_details = _load_or_fetch(
         config.save_files,
         config.paths_file,
-        open_spec.__editor.extract_paths_details,
+        editor.extract_paths_details,
     )
     parameters = _load_or_fetch(
         config.save_files,
@@ -37,17 +36,19 @@ def __load_data(open_spec: "OpenSpec"):
     requestBodies = _load_or_fetch(
         config.save_files,
         config.request_body_file,
-        open_spec.__editor.extract_request_bodies,
+        editor.extract_request_bodies,
     )
 
     responses = _load_or_fetch(
         config.save_files,
         config.responses_file,
-        open_spec.__editor.extract_responses,
+        editor.extract_responses,
     )
     overrides = load_file(config.override_file)
-    spec_files_data = open_spec.__editor.load_snippet_files()
+    spec_files_data = editor.load_snippet_files()
     #
+
+    print(51, "loader", OasBuilder.data, id(OasBuilder.data))
     data = merge_recursive(
         [
             overrides,
@@ -74,3 +75,6 @@ def _clean_invalid_paths(data):
             del data_paths[path]
     data["paths"] = data_paths
     return data
+
+
+_OpenSpec__load_data = __load_data
