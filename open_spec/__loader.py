6bd82ab2ac14721a9cba9e08a278c56b66b04d1b
Ahmad Yahia <tabebqena@gmail.com>
2021-08-08 23:15:21 +0300

    pasrse star methods

--- open_spec/__loader.py
+++ open_spec/__loader.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
-from typing import TYPE_CHECKING
+from typing import Dict, TYPE_CHECKING
 
-from ._parameters import extract_path_parameters, get_app_paths
+from ._parameters import (
+    extract_path_parameters,
+    get_app_paths,
+    VALID_METHODS_OPENAPI_V3,
+)
 from ._utils import load_file, merge_recursive
 from .builder import OasBuilder
 
@@ -47,12 +51,11 @@ def __load_data(config: "OasConfig", editor: "TemplatesEditor"):
     overrides = load_file(config.override_file)
     spec_files_data = editor.load_snippet_files()
     #
-
+    builder_data = _parse_star_method(config, OasBuilder.data)
     data = merge_recursive(
         [
             overrides,
-            # decorators_data,
-            OasBuilder.data,
+            builder_data,
             spec_files_data,
             paths_details,
             parameters,
@@ -62,6 +65,8 @@ def __load_data(config: "OasConfig", editor: "TemplatesEditor"):
         ]
     )
     data = _clean_invalid_paths(data)
+    data = _clean_invalid_request_bodies(data)
+
     return data
 
 
@@ -73,6 +78,39 @@ def _clean_invalid_paths(data):
         if path not in app_paths_list:
             del data_paths[path]
     data["paths"] = data_paths
+    return data
+
+
+def _parse_star_method(config: "OasConfig", data: Dict[str, Dict]):
+    app_allowed_methods = config.allowed_methods
+    app_paths_list = get_app_paths()
+    data_paths = deepcopy(data.get("paths", {}))
+    for path in data_paths:
+        path_value = data_paths[path]
+        keys = path_value.keys()
+        if "*" in keys:
+            allowed_methods = [
+                method
+                for method in app_paths_list.get(path, [])
+                if method in app_allowed_methods
+            ]
+
+            for method in allowed_methods:
+                if method in keys:
+                    continue
+                data_paths[path][method] = data_paths[path]["*"]
+            del data_paths[path]["*"]
+    data["paths"] = data_paths
+    return data
+
+
+def _clean_invalid_request_bodies(data: Dict[str, Dict[str, Dict]]):
+    paths = data.get("paths", {})
+    for path, path_obj in paths.items():
+        for method in ["get", "delete", "head"]:
+            if path_obj.get(method, {}).get("requestBody", None):
+                del paths[path][method]["requestBody"]
+    data["paths"] = paths
     return data
 
 
